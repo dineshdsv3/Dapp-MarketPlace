45f1571bc83d4248c4a55b7121b6b93d84589223
dineshdsv3 <dineshdsv3@gmail.com>
2019-10-09 14:08:08 +0530

    added Smart contract with the product creation

--- src/contracts/MaretPlace.sol
+++ src/contracts/MaretPlace.sol
@@ -2,8 +2,31 @@ pragma solidity ^0.5.0;
 
 contract MarketPlace {
     string public name;
+    uint public productCount;
+    mapping(uint => Product) public products;
 
-    constructor() public {
-        name = "Testing name";
+    struct Product{
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
     }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(_name).length > 0,"Invalid name");
+        require(_price > 0,"Invalid Price");
+        productCount++;
+        products[productCount] = Product(productCount,_name,_price,msg.sender,false);
+        emit ProductCreated(productCount,_name,_price,msg.sender,false);
+    }
+    
 }
